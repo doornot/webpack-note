580a90368e8d1c923b9e3e305dd801a1eda57208
wangbiao <wangbiao007@lianjia.com>
2019-01-12 20:45:15 +0800

    update server.js

--- WebpackDevMiddleware/server.js
+++ WebpackDevMiddleware/server.js
@@ -2,22 +2,20 @@ const express = require('express');
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
-const devWebpackConfig = require('./webpack.config.dev.js');
-const prodWebpackConfig = require('./webpack.config.prod.js');
+const config = require('./webpack.config.dev.js');
+
+// console.log('===process.env.NODE_ENV===', process.env.NODE_ENV);
 
 const app = express();
-const config = (process.env.NODE_ENV === 'development' ? devWebpackConfig : prodWebpackConfig);
 const compiler = webpack(config);
 
-if (process.env.NODE_ENV === 'development') {
-  // Tell express to use the webpack-dev-middleware and use the webpack.config.js
-  // configuration file as a base.
-  app.use(webpackDevMiddleware(compiler, {
-    publicPath: config.output.publicPath
-  }));
-  
-  app.use(webpackHotMiddleware(compiler));
-}
+// Tell express to use the webpack-dev-middleware and use the webpack.config.js
+// configuration file as a base.
+app.use(webpackDevMiddleware(compiler, {
+  publicPath: config.output.publicPath
+}));
+
+app.use(webpackHotMiddleware(compiler));
 
 // Serve the files on port 3000.
 app.listen(3000, function () {
